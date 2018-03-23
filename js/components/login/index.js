eb142c6e4690e6f39537d5ce09b8fa6d0f4d866b
matt <matt_jin@163.com>
2018-03-23 11:32:44 +0800

    update login view 样式

--- js/components/login/index.js
+++ js/components/login/index.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
-import { Dimensions, View, Image, StyleSheet, TextInput, Button, Text } from "react-native";
+import { Dimensions, View, Image, StyleSheet, TextInput, Button, Text, TouchableOpacity } from "react-native";
 import { login } from '../../actions';
 
 const resetAction = NavigationActions.reset({
@@ -71,26 +71,44 @@ class LoginComponent extends React.Component {
             source={require("./img/logo.png")}
             style={styles.logo}
           />
-          <View>
+          <View style={[styles.phoneRow, styles.loginBorder]}>
+            <Image
+              source={require("./img/icon-phone.png")}
+              style={styles.inputIcon}
+            />
             <TextInput
-            onChangeText={text => this.handleSPhoneChange(text)}
-              style={{height: 49, borderWidth: 0}}
+              onChangeText={text => this.handleSPhoneChange(text)}
+              style={styles.loginInput}
               placeholder="请输入手机号码"
             />
           </View>
-          <View>
-            <TextInput
-              style={{height: 49, borderWidth: 0}}
-              onChangeText={text => this.handleSmsCodeChange(text)}
-              placeholder="请输入验证码"
-            />
-          </View>
-          <Button
-            title="登录"
-            onPress={() => {
+          <View style={styles.smsCodeRow}>
+            <View style={[styles.smsCode, styles.loginBorder]}>
+              <Image
+                source={require("./img/icon-verifying.png")}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.loginInput}
+                onChangeText={text => this.handleSmsCodeChange(text)}
+                placeholder="请输入验证码"
+              />
+            </View>
+            <TouchableOpacity onPress={() => {
               this.handleLogin();
-            }}
-          />
+            }}>
+              <View style={styles.smsCodeBtn}>
+                <Text style={{fontSize:12, color:'#fff'}}>获取验证码</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity onPress={() => {
+              this.handleLogin();
+            }}>
+            <View style={styles.btn}>
+              <Text style={{fontSize:16, color:'#fff'}}>登录</Text>
+            </View>
+          </TouchableOpacity>
           <Text style={styles.notice}>新用户登录即同意<Text style={styles.noticeStrong}>《钱家用户注册协议》</Text></Text>
       </View>
     );
@@ -104,9 +122,34 @@ LoginComponent.navigationOptions = {
 /* StyleSheet =============================================================== */
 
 const styles = StyleSheet.create({
-  loginRow: {
-  },
   loginContainer: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  loginRow: {
+    alignSelf: 'stretch',
+    flexDirection:"row",
+    alignItems: "center",
+  },
+  loginBorder: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#e5e5e5"
+  },
+  phoneRow: {
+    alignSelf: 'stretch',
+    flexDirection:"row",
+    alignItems: "center",
+  },
+  smsCodeRow: {
+    width: Dimensions.get('window').width,
+    marginBottom: 40,
+    flexDirection:"row",
+    alignItems: "center",
+  },
+  smsCode: {
+    flex: 1,
+    flexDirection:"row",
     alignItems: "center",
   },
   logo: {
@@ -116,6 +159,26 @@ const styles = StyleSheet.create({
     marginTop: 59,
     marginBottom: 35,
   },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  loginInput: {
+    height: 49,
+    borderWidth: 0,
+    fontSize: 16,
+  },
+  smsCodeBtn: {
+    width: 90,
+    height: 30,
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: '#ccc',
+    borderRadius: 4,
+    alignSelf: 'flex-end',
+  },
   notice: {
     marginTop: 15,
     color: '#999',
@@ -123,6 +186,14 @@ const styles = StyleSheet.create({
   },
   noticeStrong: {
     color: '#388BED'
+  },
+  btn: {
+    width: 320,
+    height: 44,
+    backgroundColor: '#049bff',
+    alignItems: "center",
+    justifyContent: 'center',
+    borderRadius: 50,
   }
 });
   
