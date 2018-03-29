eddcaa7af56a35af87fe1f20d56b5b7f84ffbd2b
leijingyun <leijingyun@ccntjf.com>
2018-03-29 11:34:20 +0800

    设置页面

--- js/components/setting/index.js
+++ js/components/setting/index.js
@@ -1,19 +1,32 @@
 "use strict";
 
 import React from "react";
-import { Dimensions, AppRegistry, View, Text, StyleSheet, Image} from "react-native";
+import { Dimensions, AppRegistry, View, Text, StyleSheet, Image, Button, Switch} from "react-native";
 
 class SettingComponent extends React.Component {
+
+    state = {
+        value: false,
+        disabled: false,
+    }
+
     render() {
         return (
             <View>
                 <View style={styles.versionInfo}>
-                    <Text style={styles.version}>版本信息</Text>
+                    <Text style={styles.settingInfo}>版本信息</Text>
                     <Image source={require('./img/icon-arrownext.png')} />
                 </View>
                 <View style={styles.closeNeed}>
-                    <Text style={styles.close}>关闭投资需求</Text>
-                    <Image source={require('./img/icon-arrownext.png')} />
+                    <Text style={styles.settingInfo}>关闭投资需求</Text>
+                    <View style={styles.switchButton}>
+                        <Switch  onTintColor='#43A4FF' thumbTintColor='#fff' value={this.state.value} 
+                            onValueChange={(value)=>{
+                                this.setState({
+                                    value:value,
+                                });}} >
+                        </Switch>
+                    </View>
                 </View>
             </View>
         );
@@ -32,32 +45,33 @@ const styles = StyleSheet.create({
         marginTop: 21,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
+        paddingLeft:15,
+        paddingRight:15,
+        justifyContent: 'space-between',
     },
     closeNeed: {
         backgroundColor: '#fff',
         marginTop: 21,
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft:15,
+        paddingRight:15,
         alignItems: 'center',
-        justifyContent: 'center',
     },
-    version: {
-        width: 325,
+    settingInfo: {
+        // width: 300,
         height: 44,
         lineHeight: 44,
         color: '#666',
         fontSize: 15,
     },
-    close: {
-        width: 325,
-        height: 44,
-        lineHeight: 44,
-        color: '#666',
-        fontSize: 15,
+    switchButton: {
+       width: 44,
+       height: 25,
     },
 });
 
-// AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
 
 /* exports ================================================================== */
 module.exports = SettingComponent;
